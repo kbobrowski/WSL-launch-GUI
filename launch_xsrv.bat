a8934e0247b59ce035f3e2eb71dff1a4f1aa1412
EsuS <31799121+dotEsuS@users.noreply.github.com>
2018-03-17 13:43:21 +0100

    Added condition to check if VcXsrv is already running
    If VcXsrv is running no second instance will be launched (which would result in an error message).

--- launch_xsrv.bat
+++ launch_xsrv.bat
@@ -4,6 +4,13 @@ set xsrvexec=%1
 set dispnum=%2
 set windowmode=%3
 
+:start
+tasklist /FI "IMAGENAME eq VcXsrv.exe" 2>NUL | Find /I /N "VcXsrv.exe" > NUL 
+if %errorlevel%==0 goto end 
 start "Xserver" /D "%programfiles%\VcXsrv" %xsrvexec% :%dispnum% -ac -terminate -lesspointer -%windowmode% -clipboard -wgl
-
+echo starting server 
+goto exit
+:end
+goto exit
+:exit
 exit
